7bd1bf91ca46ebf9de18b37335b492ee81ec4b2c
torresv1 <torres_vladimir@hotmail.com>
2014-09-12 19:28:16 -0400

    Correction in the "fields" object

--- odoo/vt_vetclinic/vetclinic.py
+++ odoo/vt_vetclinic/vetclinic.py
@@ -1,9 +1,9 @@
-from openerp.osv import osv, fields
+from osv import osv, fields
 
 class vetclinic_animal(osv.Model):
     _name = "vetclinic.animal"
     _columns = {
-        'name': field.char('Name', size=64),
-        'birthday': field.date('Birth Date'),
+        'name': fields.char('Name', size=64),
+        'birthday': fields.date('Birth Date'),
 }
     
