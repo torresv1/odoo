8a7c454fd7f8d94a115f1d798a97e9930a25bb37
torresv1 <torres_vladimir@hotmail.com>
2014-09-23 15:09:49 -0400

    None

--- odoo/vt_vetclinic/vetclinic.py
+++ odoo/vt_vetclinic/vetclinic.py
@@ -5,5 +5,17 @@ class vetclinic_animal(osv.Model):
     _columns = {
         'name': fields.char('Name', size=64),
         'birthday': fields.date('Birth Date'),
+        'classication_id': fields.many2one('vetclinic.classification','Classification')
 }
-    
+
+class vetclinic_classification(osv.Model):
+    _name = "vetclinic.classification"
+    _columns = {
+        'name': fields.char('Name', size=32),  
+        }
+
+class vetclinic_breed(osv.Model):
+    _name = "vetclinic.breed"
+    _columns = {
+        'name': fields.char('Name', size=32),  
+        }    
