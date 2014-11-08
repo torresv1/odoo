a5a430f91b57fa53b03acd16ca7736bf68cb5913
torresv1 <torres_vladimir@hotmail.com>
2014-11-08 10:14:42 -0500

    updating

--- odoo/boatcompany/boatcompany.py
+++
@@ -1,8 +0,0 @@
-from openerp.osv import osv, fields
-
-class product_product(osv.osv):
-    _inherit = "product.product"
-    _columns ={
-        'boatlength': fields.char("Boat Length", size=10),
-        'fuelcapacity':fields.char("Fuel Capacity", size=10),                 
-               }
