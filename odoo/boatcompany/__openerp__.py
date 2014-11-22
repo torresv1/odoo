0a8cfad779b11cd9e7cea8641139350704cd80ba
torresv1 <torres_vladimir@hotmail.com>
2014-11-22 12:20:02 -0500

    more

---
+++ odoo/boatcompany/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    "name" : "Boat Company",
+    "version" : "1.0",
+    "author" : "Vladimir Torres-Rivas",
+    'complexity': "easy",
+    "description" : """
+    Boat Company Application
+    """,
+    "website" : "http://www.yugla.com",
+    "depends" : ['product'],
+    "category" : "Stuff",
+    "sequence": 18,
+    "demo" : [],
+    "data" : [
+        'boatcompany_view.xml',
+        ],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'active': False,
+}
---
+++ odoo/boatcompany/boatcompany.py
@@ -0,0 +1,14 @@
+from openerp.osv import osv, fields
+
+
+class product_template(osv.osv):
+    _name = "product.template"
+    _inherit = "product.template"
+    _columns = {
+        'boatlength':fields.char("Boat Length", size = 10),
+        'fuelcapacity': fields.char("Fuel Capacity",size=10),
+
+
+    }
+
+product_template()
