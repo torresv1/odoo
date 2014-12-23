56663eaf2c61595ac406be74a10fb5a8b482d4ba
torresv1 <torres_vladimir@hotmail.com>
2014-12-22 19:04:10 -0500

    hgf

--- odoo/boatcompany/boatcompany.py
+++ odoo/boatcompany/boatcompany.py
@@ -9,21 +9,19 @@ class product_template(osv.osv):
         'fuelcapacity': fields.char("Fuel Capacity",size=10),
         'modeloptions_id': fields.many2one("product.category","Model Options"),
     }
+
+class product_product(osv.osv):
+    _name = "product.product"
+    _inherit = "product.product"
+
     def search(self,cr,uid,args,offset=0,limit=None, order=None, context=None,count=False):
         if context is None:
             context ={}
-            
-            """ Check to see if we get have the ‘boatmodel_id’ passed in context.  
-            This will keep the search from breaking when we are using it for searches outside
-            of the sales order form."""  
-                
         if context.get('boatmodel_id'):
-            
             #Code to limit by category assigned to the boat model.
-            
-            productobj =self.pool.get('product.product').read(cr,uid,context['boatmodel_id'],['modeloptions_id']) 
-          
-        return super(product_template, self).search(cr,uid,args,offset,limit,order,context=context,count=count)
+            productobj =self.pool.get('product.product').read(cr,uid,context['boatmodel_id'],['modeloptions_id'])
+            args = [('categ_id','=',productobj['modeloptions_id'][0])] + args
+        return super(product_product, self).search(cr,uid,args,offset,limit,order,context=context,count=count)
 
 class sale_order(osv.osv):
     _inherit = "sale.order"
